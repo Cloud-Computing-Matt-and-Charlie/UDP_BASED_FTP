9277452a3891b44d6836aca8dbfc12aa95c68341
Matt-Pisini <pisini@usc.edu>
2020-09-15 15:06:02 -0700

    Add files via upload

---
+++ server.cc
@@ -0,0 +1,12 @@
+/******************************************************//*
+Creators: Matthew Pisini, Charles Bennett
+Date: 9/19/20
+
+Description:
+Inputs: 
+1. IP address of the client
+2. Port number of the client
+3. Path to the directory of the file to be sent to client.
+
+*//******************************************************/
+
