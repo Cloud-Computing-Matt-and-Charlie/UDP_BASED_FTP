2a00eb87ae0c1f4470ce3c086a443a017f1ca1eb
charlie-bennett <35985336+charlie-bennett@users.noreply.github.com>
2020-09-15 21:26:13 -0700

    Update udp.h

--- udp.h
+++ udp.h
@@ -1,12 +1,23 @@
+#ifndef UDP_H
+#define UDP_H
+
+#define PACKET_SIZE 10000
+
 class UDP
 {
 public:
-	struct addrinfo hints, *my_address, *servinfo, *A_address, *B_address;
-  int sock_fd; 
-  int rv; 
+	struct addrinfo hints, *my_address, *servinfo, *dest_address;
+	int sock_fd; 
+	int rv;
+	int listen_port; 
+	int dest_port; 
+	int desig; 
+	
 	map<string, addrinfo*> addresses;
-	UDP(); 
-	int send(string server_ID, int message_size); 
-	int recieve(); 
+	UDP(char* dest_ip_address, int listen_port, int dest_port); 
+	int send(char* buffer, int message_size); 
+	int recieve(char* buffer, int& message_size); 
 	~UDP(); 
 };
+
+#endif
