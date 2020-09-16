b557c4d5339961f6a0b8911317512ae1da3d071d
charlie-bennett <35985336+charlie-bennett@users.noreply.github.com>
2020-09-15 19:39:44 -0700

    Update packet_dispenser.cpp

--- packet_dispenser.cpp
+++ packet_dispenser.cpp
@@ -16,10 +16,12 @@ PacketDispenser::PacketDispenser(vector<string> raw_input_data) : input_data{raw
     auto start = std::chrono::system_clock::now();
     this->total_start = std::chrono::system_clock::to_time_t(start);
     this->last_packet_time = this->total_start; 
-    pthread_mutex_init(&pop_lock); 
-    pthread_mutex_init(&push_lock); 
-    pthread_muxtex_unlock(&pop_lock); 
-    pthread_mutex_unlock(&push_lock); 
+    pthread_mutex_init(&this->pop_lock); 
+    pthread_mutex_init(&this->push_lock); 
+    pthread_mutex_init(&this->ack_lock); 
+    pthread_muxtex_unlock(&this->pop_lock); 
+    pthread_mutex_unlock(&this->push_lock); 
+    pthread_mutex_unlock(&this->ack_lock); 
 }
 
 double PacketDispenser::getTimeSinceLastPacket()
@@ -62,44 +64,41 @@ void PacketDispenser::setMaxBandwidth(int max_bandwidth_in)
   this->max_bandwidth = max_bandwidth_in; 
   this->min_diff_time = 1/((double)max_bandwidth_in); 
 }
-
-string Packet 
-  
-  
-  
-  
-
-/*
-
-class PacketDispenser
+void PacketDispenser::getAckDistance()
 {
-private: 
-  pthread_mutex_t pop_lock; 
-  pthread_mutex_t push_lock; 
-  vector<string> input_data; 
-  vector<int> is_acked; 
-  
-  struct queue_node(string payload, int index) 
-  {
-    string data = payload; 
-    int sequence_number = index; 
-  };
-  queue<*queue_node> packet_queue; 
-  
-  int window_size = 20; //how long until we start re-queueing 
-public: 
-  PacketDispenser(vector<string> raw_input_data) : input_data{raw_input_data}
-  {
-    this->is_acked = vector<int>(input_data.size(), 0); 
-    queue_node* temp; 
-    int count = 0; 
-    //enqueue the list 
-    for (auto entry : this->input_data)
+    for (int i = this->is_acked.size()-1; i>=0; i--)
     {
-      temp = new queue_node(entry, count++);  
-      this->packet_queue.push(temp); 
+        if (is_acked[i]) 
+        {
+            int last_acked = i + 1; 
+            break; 
+        }
     }
-
-  */  
+    return packets_sent - last_acked; 
+}
+void PacketDispenser::putAck(int sequence_number)
+{
+    pthread_mutex_lock(&ack_lock); 
+    this->is_acked(sequence_number) = 1; 
+    pthread mutex_unlock(&ack_lock); 
+}
+void PacketDispenser::resendInRange(int begin, int end)
+{
+    pthread_mutex_lock(&push_lock); 
+    queue_node* temp; 
+    for (int i = begin; i<end+1; i++)
+    {
+        if (!is_acked[i])
+        {
+            temp = new queue_node(input_data[i], i); 
+            packet_queue.push(temp); 
+        }
+    }
+    pthread_mutex_unlock(&push_lock); 
+}
+    
+    
+  
+  
     
     
