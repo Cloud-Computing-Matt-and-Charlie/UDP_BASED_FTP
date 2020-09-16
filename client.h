9010d35d1776c4443a7c6eeeab3b13f8ba9ad73a
matt-pisini <pisini@usc.edu>
2020-09-16 14:07:33 -0700

    no compilation errors UDP.cpp and client.cpp

--- client.h
+++ client.h
@@ -1,6 +1,9 @@
 #ifndef CLIENT
 #define CLIENT
 
+#include "UDP.h"
+#include <vector>
+
 class client_listen : public UDP
 {
 public:
@@ -8,17 +11,22 @@ public:
     char *data_array;
     int num_packets_expected, packet_size, array_size;
     bool first_packet;
-    vector<int> packet_ID_list;
+    std::vector<int> packet_ID_list;
     int packet_ID_list_size;
+    pthread_mutex_t mem_lock;
 
     //functions
-    client_listen(char* dest_ip_address, int listen_port, int dest_port);
+    // client_listen(char* dest_ip_address, int listen_port, int dest_port):UDP(dest_ip_address,listen_port,dest_port){};
+    client_listen(char* dest_ip_address, char * listen_port, int dest_port);
     void create_array(int packet_size, int num_packets);
     void array_add(char * data, int packet_offset, int data_size);
     void print_data_array();
     int strip_header(char * data);
-    void process_packet(char * data, int size);
-    ~client();
+    void control_packet(char * data);
+    // void process_packet(char * data, int size);
+    static void * do_processing(void * arg);
+    void * process_packet(void * packet_struct);
+    // ~client_listen();
 };
 
 #endif
