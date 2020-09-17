43c5be39a6b73ef29e66357f4dfe9724ba5830ba
charlie-bennett <35985336+charlie-bennett@users.noreply.github.com>
2020-09-16 20:54:21 -0700

    Update and rename server.cc to server.cpp

--- server.cpp
+++ server.cpp
@@ -10,3 +10,25 @@ Inputs:
 
 *//******************************************************/
 
+
+int reciever_thread(
+
+int main(int argc, char ** argv)
+{
+  
+  
+  if (argc != 4)
+  {
+    cout<<endl<<"Invalid Input"<<endl;
+    return 0; 
+  } 
+  
+  char* Client_IP_Address = argv[1]; 
+  char* Client_Port_Num = argv[2]; 
+  char* File_Path = argv[3]; 
+  
+  
+  
+    
+    
+   
