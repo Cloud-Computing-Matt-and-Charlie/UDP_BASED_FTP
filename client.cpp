7ec16a05e7ffc4e27d477a7f43f5489ddd679497
matt-pisini <pisini@usc.edu>
2020-09-18 13:03:00 -0700

    fixed UDP

--- client.cpp
+++ client.cpp
@@ -25,7 +25,7 @@ Inputs:
 #define HEADER_SIZE (2)
 #define FIELD1_SIZE (2)
 #define DEST_PORT "10001"
-#define DEST_IP "127.0.0.1"
+#define DEST_IP "192.168.86.152"
 #define PACKET_SIZE (1500)
 #define NUM_ACKS (150)
 #define ACK_WINDOW (5)
@@ -110,7 +110,7 @@ int client_listen::strip_header(vector<char> data)
         ACK_input.push_back(data[i]);
     }
     int packet_ID = bytes_to_int(input, HEADER_SIZE);
-    cout << "strip header val: " << packet_ID << endl;
+    // cout << "strip header val: " << packet_ID << endl;
     this->packet_ID_list.push(ACK_input);
     this->packet_ID_list_size++;
     return packet_ID;
@@ -202,7 +202,8 @@ void client_listen::send_ACKs(int index)
             cout << j << endl;
         }
         cout << endl;
-        // this->send(output);
+        this->setPacketSize(NUM_ACKS*HEADER_SIZE);
+        this->send((char *)output);
     }
 }
 
@@ -211,6 +212,9 @@ void listener(const char* dest_ip_address, char* listen_port, char* dest_port)
     client_listen client((char*)dest_ip_address, listen_port, dest_port);
     int thread_num, byte_size;
     pthread_t processing_thread;
+    char * things = "123456789";
+    client.setPacketSize(9);
+    client.send(things);
     cout << "creating thread..." << endl;
     thread_num = pthread_create(&processing_thread, NULL, &empty_packet_queue, (void*)&client);
     int count = 0;
@@ -254,8 +258,8 @@ int main(int argc, char const* argv[])
         cout << "need to supply listening port" << endl;
         exit(1);
     }
-    char temp_char = '\0';
-    listener((const char*)DEST_IP, (char*)argv[1], &temp_char);
+    // char temp_char = '\0'; 
+    listener((const char*)DEST_IP, (char*)argv[1], (char *)DEST_PORT);
     // fstream file;
     // int socket_fd, client_length, input_length;
     // struct sockaddr_in client, source;
