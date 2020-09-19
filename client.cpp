2b7145a676676ddd964c7d7cea2a172f39da485d
Charles Bennett <cfbennet@usc.edu>
2020-09-19 16:52:29 -0700

    still out here working

--- client.cpp
+++ client.cpp
@@ -31,10 +31,10 @@ Inputs:
 #define FIELD1_SIZE (2)                            //Packet Size
 #define FIELD2_SIZE (2)                            //# of Packets in Transmission
 #define NUM_CONTROL_FIELDS (2)                     //# Fields in control header
-#define NUM_PACKETS_EXPECTED (100)                 //Hardcoded Packet Size (comment if control packet in use)
+#define NUM_PACKETS_EXPECTED (910)                 //Hardcoded Packet Size (comment if control packet in use)
 int control_field_array[NUM_CONTROL_FIELDS];       //Array to store the decoded control fields
 int control_field_sizes[NUM_CONTROL_FIELDS]        //Define sizes of control fields
-= {FIELD1_SIZE, FIELD1_SIZE};  
+    = {FIELD1_SIZE, FIELD1_SIZE};
 
 /**************************************************/
 
@@ -61,8 +61,8 @@ client_listen::client_listen(char* dest_ip_address, char* listen_port, char* des
 void client_listen::map_add(int packet_number, vector<char> data)
 {
     vector<char> payload = data;
-    payload.erase(payload.begin(), payload.begin()+2);
-    if(!this->data_map.count(packet_number))
+    payload.erase(payload.begin(), payload.begin() + 2);
+    if (!this->data_map.count(packet_number))
     {
         this->num_packets_received++;
     }
@@ -76,13 +76,13 @@ void client_listen::print_data_map()
     cout << "(packet ID, data)" << endl;
     for (const auto& x : this->data_map)
     {
-        
+
         cout << x.first << ": ";// << x.second << endl;
         vector<char> items = x.second;
         for (it = items.begin(); it != items.end(); it++)
         {
             cout << *it;
-        } 
+        }
         cout << endl;
     }
 }
@@ -91,7 +91,7 @@ void client_listen::print_data_map()
 /************************************** PACKET PROCESSING ****************************************/
 void client_listen::process_packet(vector<char> packet)
 {
-    // int packet_ID = strip_header(packet);    
+    // int packet_ID = strip_header(packet);
     //strip_header
     unsigned char input[HEADER_SIZE];
     vector<char> ACK_input;
@@ -104,8 +104,8 @@ void client_listen::process_packet(vector<char> packet)
 
     //add payload to map and packet_ID_list if it is a unique packet ID
     vector<char> payload = packet;
-    payload.erase(payload.begin(), payload.begin()+2);
-    if(!this->data_map.count(packet_ID))
+    payload.erase(payload.begin(), payload.begin() + 2);
+    if (!this->data_map.count(packet_ID))
     {
         this->num_packets_received++;
         this->packet_ID_list.push(ACK_input);
@@ -175,7 +175,7 @@ void client_listen::create_ACK_packet(int ACK_packet_size)
 void client_listen::send_ACKs(int index)
 {
     int temp;
-    if(this->ACK_queue.size() <= ACK_WINDOW)
+    if (this->ACK_queue.size() <= ACK_WINDOW)
     {
         temp = (index + 1);
         index = 0;
@@ -185,38 +185,38 @@ void client_listen::send_ACKs(int index)
         temp = index + 1;
         index = (index + 1) - ACK_WINDOW;
     }
-    for (vector<vector<char>>::iterator it = (this->ACK_queue.begin()+index);
-     it != (this->ACK_queue.begin() + temp); ++it)
+    for (vector<vector<char>>::iterator it = (this->ACK_queue.begin() + index);
+            it != (this->ACK_queue.begin() + temp); ++it)
     {
         unsigned char* output;
-        output = (unsigned char *)vector_to_cstring(*it);
+        output = (unsigned char*)vector_to_cstring(*it);
         /* DEBUG
         cout << "output: ";
         int j = 0;
         for(int i = 0; i < it->size(); i+=2)
-        {   
+        {
             // j = output[i] | output[i+1] << 8;
             unsigned char f[2] = {output[i],output[i+1]};
             j = bytes_to_int(f,2);
             cout << j << endl;
         }
         cout << endl;*/
-        cout << "sending ACK Packet #: " << distance(this->ACK_queue.begin(),it) << endl;
-        this->send((char *)output);
+        cout << "sending ACK Packet #: " << distance(this->ACK_queue.begin(), it) << endl;
+        this->send((char*)output);
         delete [] output;
     }
 }
 
-void listener(char* dest_ip_address, char* listen_port, char* dest_port, char * output_file)
+void listener(char* dest_ip_address, char* listen_port, char* dest_port, char* output_file)
 {
     client_listen client(dest_ip_address, listen_port, dest_port);
     int thread_num, byte_size, rc;
-    void * status;
+    void* status;
     pthread_t processing_thread;
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    client.setPacketSize(NUM_ACKS*HEADER_SIZE);
+    client.setPacketSize(NUM_ACKS * HEADER_SIZE);
     cout << "creating processing thread..." << endl;
     thread_num = pthread_create(&processing_thread, &attr, &empty_packet_queue, (void*)&client);
     pthread_attr_destroy(&attr);
@@ -224,13 +224,13 @@ void listener(char* dest_ip_address, char* listen_port, char* dest_port, char * 
     {
         // char packet_ID[HEADER_SIZE];
         std::cout << "listening for packet..." << endl;
-        char * temp = client.recieve(byte_size);
+        char* temp = client.recieve(byte_size);
         vector<char> thread_buffer = cstring_to_vector(temp, byte_size);
 
         if (client.num_packets_received >= client.num_packets_expected) //have all the packets
         {
             rc = pthread_join(processing_thread, &status);
-            if (rc) 
+            if (rc)
             {
                 std::cout << "ERRROR: joining threads" << std::endl;
                 exit(1);
@@ -268,10 +268,10 @@ int main(int argc, char const* argv[])
         exit(1);
     }
 
-    char * DEST_IP = (char *)argv[1];
-    char * LISTEN_PORT = (char *)argv[2];
-    char * DEST_PORT = (char *)argv[3];
-    char * output_file = (char *)argv[4];
+    char* DEST_IP = (char*)argv[1];
+    char* LISTEN_PORT = (char*)argv[2];
+    char* DEST_PORT = (char*)argv[3];
+    char* output_file = (char*)argv[4];
     listener(DEST_IP, LISTEN_PORT, DEST_PORT, output_file);
     // client_listen client(DEST_IP, LISTEN_PORT, DEST_PORT);
     // vector<char> input = {'a','a','b','b'};
@@ -284,18 +284,18 @@ int main(int argc, char const* argv[])
     return 0;
 }
 
-void write_to_file(std::map<int, std::vector <char>> map, char * file_name)
-{    
+void write_to_file(std::map<int, std::vector <char>> map, char* file_name)
+{
     ofstream file;
     file.open(file_name);
     vector<char>::iterator it;
     for (const auto& x : map)
-    {        
+    {
         vector<char> items = x.second;
         for (it = items.begin(); it != items.end(); it++)
         {
             file << *it;
-        } 
+        }
     }
     file.close();
 }
@@ -313,16 +313,16 @@ void* empty_packet_queue(void* input)
             client->create_ACK_packet(NUM_ACKS);
             client->send_ACKs(index);
             index++;
-            
+
         }
         if (client->packet_queue.size() > 0) //packets still to be processed
         {
             //Have not received all of the unique packets wee expect tot receive
-            if(client->num_packets_received < client->num_packets_expected)
+            if (client->num_packets_received < client->num_packets_expected)
             {
                 vector<char> packet = client->packet_queue.front();
                 client->process_packet(packet);
-                client->packet_queue.pop();    
+                client->packet_queue.pop();
             }
 
             //Received all of the packets we expected to receive --> done processing
